d1e5bc6b7a8b651a57b340b44c297f292a8284dd
kiran <kiranrajbaburaj1992@gmail.com>
2019-01-06 11:14:50 +0530

    Add project files.

---
+++ APS/Adapter/CustomListAdapter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using APS.Model;
+
+namespace APS.Adapter
+{
+    public class CustomListAdapter : BaseAdapter<User>
+    {
+        public List<User> sList;
+        private Context sContext;
+        public CustomListAdapter(Context context, List<User> list)
+        {
+            sList = list;
+            sContext = context;
+        }
+        public void Add(User user)
+        {
+            sList.Add(user);
+            this.NotifyDataSetChanged();
+        }
+        public override User this[int position]
+        {
+            get
+            {
+                return sList[position];
+            }
+        }
+        public override int Count
+        {
+            get
+            {
+                return sList.Count;
+            }
+        }
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+            try
+            {
+                if (row == null)
+                {
+                    row = LayoutInflater.From(sContext).Inflate(Resource.Layout.layout_user, null, false);
+                }
+                TextView txtName = row.FindViewById<TextView>(Resource.Id.Name);
+                txtName.Text = sList[position].email;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+            finally { }
+            return row;
+        }
+    }
+}
---
+++ APS/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+using APS.Model;
+using System.Collections.Generic;
+using APS.Adapter;
+using System.Threading;
+
+namespace APS
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        CustomListAdapter adapter;
+        List<User> mlist;
+        private ListView listView;
+        private SynchronizationContext sc;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            listView = FindViewById<ListView>(Resource.Id.listView1);
+
+            //Adapter
+            mlist = new List<User>();
+            adapter = new CustomListAdapter(this, mlist);
+            listView.Adapter = adapter;
+            sc = SynchronizationContext.Current;
+            getJSON();
+        }
+
+        // Fetch JSON in the background
+
+        private async void getJSON()
+        {
+            // PASS URL
+            IRestClient client = new RestClient("http://jsonplaceholder.typicode.com");
+            IRestRequest request = new RestRequest("users/", Method.GET);
+
+            try
+            {
+                await Task.Run(() =>
+                {
+                    IRestResponse<List<User>> response = client.Execute<List<User>>(request);
+                    foreach(var user in response.Data)
+                    {
+                        sc.Post(new SendOrPostCallback(o =>
+                        {
+                            adapter.Add(o as User);
+                        }), user);
+                    }
+                });
+
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
